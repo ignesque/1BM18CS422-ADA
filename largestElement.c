3606209866950c61f26ad22a1e93eb8253fe3865
Ignes <ignes@pm.me>
2019-08-24 10:21:12 +0530

    Add files via upload

---
+++ largestElement.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+	void bubbleSort(int arr[],int n){
+	int temp,i,j;
+	for(i=0;i<n;i++){
+		for(j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+}
+
+void getLarge(int arr[],int n,int k){
+	int i;
+	printf("\n%d largest elements in the array are ",k);
+	for(i=n-1;i>=n-k;i--){
+		printf("%d",arr[i]);
+		printf(" ");
+	}
+}
+int main(){
+	int n,k,i;
+	int arr[30];
+	printf("Enter the range of array:");
+	scanf("%d",&n);
+	printf("Enter the elements to be inserted in the array\n");
+	for(i=0;i<n;i++)
+		scanf("%d",&arr[i]);
+	printf("\nHow many largest elements to be fetched?(Range=%d):",n);
+	scanf("%d",&k);
+	bubbleSort(arr,n);
+	getLarge(arr,n,k);
+	return 0;
+}
---
+++ leastElement.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+void leastElement(int arr[],int n,int k){
+	int i,j,temp,min;
+	for(i=0;i<k;i++){
+		min=i;
+		for(j=i+1;j<n;j++){
+			if(arr[j]<arr[min])
+				min=j;
+	
+	}
+	temp=arr[i];
+	arr[i]=arr[min];
+	arr[min]=temp;
+	}
+	cout<<k<<"th smallest element is "<<arr[k-1]<<"\n";
+}
+int main(){
+	int *arr,n,k;
+	cout<<"Enter the range of array:";
+	cin>>n;
+	arr=new int[n];
+	cout<<"Enter the elements\n";
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	cout<<"Which smallest element to find?"<<"(Range="<<n<<")\n";
+	cin>>k;
+	if(k<=n)
+	leastElement(arr,n,k);
+	else
+		cout<<"Index not found\n";
+	return 0;
+}
+
+
