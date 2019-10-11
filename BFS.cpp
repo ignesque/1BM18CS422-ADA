b748f4a6bde139c1cfe38326340e32849a3044dd
Ignes <ignes@pm.me>
2019-10-11 19:23:28 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,58 +1,39 @@
 #include<iostream> 
 #include <list> 
-
 using namespace std; 
 
-
-class Graph 
-{ 
+class Graph { 
 	int V;  
-
 	list<int> *adj; 
 public: 
 	Graph(int V); 
-
-
 	void addEdge(int v, int w); 
-
 	void BFS(int s); 
 }; 
 
-Graph::Graph(int V) 
-{ 
+Graph::Graph(int V) { 
 	this->V = V; 
 	adj = new list<int>[V]; 
 } 
 
-void Graph::addEdge(int v, int w) 
-{ 
+void Graph::addEdge(int v, int w) { 
 	adj[v].push_back(w);  
 } 
 
-void Graph::BFS(int s) 
-{ 
-
+void Graph::BFS(int s) { 
 	bool *visited = new bool[V]; 
 	for(int i = 0; i < V; i++) 
 		visited[i] = false; 
-
 	list<int> queue; 
-
 	visited[s] = true; 
 	queue.push_back(s); 
-
 	list<int>::iterator i; 
-
-	while(!queue.empty()) 
-	{ 
+	while(!queue.empty()) { 
 		s = queue.front(); 
 		cout << s << " "; 
 		queue.pop_front(); 
-
-		for (i = adj[s].begin(); i != adj[s].end(); ++i) 
-		{ 
-			if (!visited[*i]) 
-			{ 
+		for (i = adj[s].begin(); i != adj[s].end(); ++i) { 
+			if (!visited[*i]) { 
 				visited[*i] = true; 
 				queue.push_back(*i); 
 			} 
@@ -60,8 +41,7 @@ void Graph::BFS(int s)
 	} 
 } 
 
-int main() 
-{ 
+int main() { 
     int n,s,d,node;
     cout<<"Enter the number of vertices:";
     cin>>n;
