05d44ae0bb7a1e27867e49c2629bbd4e33531639
Ignes <ignes@pm.me>
2019-10-21 19:59:04 +0530

    modified:   N_Queen.c

--- N_Queen.c
+++ N_Queen.c
@@ -2,33 +2,41 @@
 int N;
 int board[100][100];
 
-int is_attack(int i,int j){
-    int k,l;
-    for(k=0;k<N;k++){
-        if((board[i][k] == 1) || (board[k][j] == 1))
-            return 1;
-    }
-   for(k=0;k<N;k++){
-       for(l=0;l<N;l++){
-            if(((k+l) == (i+j)) || ((k-l) == (i-j)))
-            {
-                if(board[k][l] == 1)
-                    return 1;
-            }
-        }
-    }
-    return 0;
+int is_attack(int i, int j)
+{
+   int k, l;
+   for (k = 0; k < N; k++)
+   {
+      if ((board[i][k] == 1) || (board[k][j] == 1))
+         return 1;
+   }
+   for (k = 0; k < N; k++)
+   {
+      for (l = 0; l < N; l++)
+      {
+         if (((k + l) == (i + j)) || ((k - l) == (i - j)))
+         {
+            if (board[k][l] == 1)
+               return 1;
+         }
+      }
+   }
+   return 0;
 }
 
-int N_queen(int n){
-   int i,j;
-   if(n==0)
+int N_queen(int n)
+{
+   int i, j;
+   if (n == 0)
       return 1;
-   for(i=0;i<N;i++){
-      for(j=0;j<N;j++){
-         if((!is_attack(i,j)) && (board[i][j]!=1)){
+   for (i = 0; i < N; i++)
+   {
+      for (j = 0; j < N; j++)
+      {
+         if ((!is_attack(i, j)) && (board[i][j] != 1))
+         {
             board[i][j] = 1;
-            if(N_queen(n-1)==1)
+            if (N_queen(n - 1) == 1)
                return 1;
             board[i][j] = 0;
          }
@@ -37,21 +45,23 @@ int N_queen(int n){
    return 0;
 }
 
-int main(){
+int main()
+{
    printf("Enter the size of board:");
-   scanf("%d",&N);
-   int i,j;
-   for(i=0;i<N;i++)
-      for(j=0;j<N;j++)
-         board[i][j]=0;
+   scanf("%d", &N);
+   int i, j;
+   for (i = 0; i < N; i++)
+      for (j = 0; j < N; j++)
+         board[i][j] = 0;
    N_queen(N);
    printf("\n");
-   for(i=0;i<N;i++){
-      for(j=0;j<N;j++)
-         if(board[i][j]==1)
-         printf("q ");
+   for (i = 0; i < N; i++)
+   {
+      for (j = 0; j < N; j++)
+         if (board[i][j] == 1)
+            printf("q ");
          else
-         printf(". ");
+            printf(". ");
       printf("\n");
    }
 }
